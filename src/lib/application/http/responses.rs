1b32f43b4d8f18b6f3d542447d354368107fd12a
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-13 08:10:16 +0100

    chore: Implement From trait for MemberError in ApiResponseError

--- src/lib/application/http/responses.rs
+++ src/lib/application/http/responses.rs
@@ -30,3 +30,15 @@ pub struct ApiResponseError {
     pub status: u16,
     pub message: String,
 }
+
+impl From<MemberError> for ApiError {
+  fn from(value: MemberError) -> Self {
+    match value {
+      MemberError::NotFound => ApiError::NotFound("Member not found".to_string()),
+      MemberError::Unauthorized => ApiError::Unauthorized("Unauthorized".to_string()),
+      MemberError::Forbidden => ApiError::Forbidden("Forbidden".to_string()),
+      MemberError::CreateError(e) => ApiError::InternalServerError(e),
+      MemberError::DeleteError(e) => ApiError::InternalServerError(e),
+    }
+  }
+}
