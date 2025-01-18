0e80279ec9fbde1a9789c737caed5de7b91948db
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-19 00:10:18 +0100

    chore: Update MemberService and MemberRepository with guild_id parameter in create method

--- src/lib/domain/member/ports.rs
+++ src/lib/domain/member/ports.rs
@@ -12,7 +12,17 @@ pub trait MemberService: Clone + Send + Sync + 'static {
         &self,
         id: &str,
     ) -> impl Future<Output = Result<Vec<Member>, MemberError>> + Send;
-    fn create(&self, user_id: String) -> impl Future<Output = Result<Member, MemberError>> + Send;
+    fn create(
+        &self,
+        user_id: String,
+        guild_id: String,
+    ) -> impl Future<Output = Result<Member, MemberError>> + Send;
+    fn add_role(
+        &self,
+        guild_id: String,
+        user_id: String,
+        role_id: String,
+    ) -> impl Future<Output = Result<(), MemberError>> + Send;
 }
 
 pub trait MemberRepository: Clone + Send + Sync + 'static {
@@ -24,9 +34,24 @@ pub trait MemberRepository: Clone + Send + Sync + 'static {
         &self,
         id: &str,
     ) -> impl Future<Output = Result<Option<Member>, MemberError>> + Send;
+    fn find_by_user_id_and_guild_id(
+        &self,
+        user_id: &str,
+        guild_id: &str,
+    ) -> impl Future<Output = Result<Member, MemberError>> + Send;
     fn find_by_guild_id(
         &self,
         guild_id: &str,
     ) -> impl Future<Output = Result<Vec<Member>, MemberError>> + Send;
-    fn create(&self, user_id: String) -> impl Future<Output = Result<Member, MemberError>> + Send;
+    fn create(
+        &self,
+        user_id: String,
+        guild_id: String,
+    ) -> impl Future<Output = Result<Member, MemberError>> + Send;
+    fn add_role(
+        &self,
+        guild_id: String,
+        user_id: String,
+        role_id: String,
+    ) -> impl Future<Output = Result<Member, MemberError>> + Send;
 }
